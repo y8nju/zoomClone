4805a6947a0745a0941ffe36b02bff4d88b5b0ce
y8nju <89aruc@gmail.com>
2023-01-18 17:34:31 +0900

    Setup: Front

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
-console.log('hello');
+// console.log(__dirname); //C:\ARU\zoomClone\src
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname+ "/public"));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(8000);
+const handleListen = () => console.log(`Listening on http://localhost:8000`);
+
+app.listen(8000, handleListen);
