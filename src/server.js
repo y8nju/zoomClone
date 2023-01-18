2a7c2977aadd574d1051d6394480f141a2507b21
y8nju <89aruc@gmail.com>
2023-01-18 22:19:57 +0900

    Add: WebSocket

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -6,8 +8,14 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname+ "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:8000`);
 
-app.listen(8000, handleListen);
+// app.listen(8000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+
+server.listen(8000);
