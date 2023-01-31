581177918f4a18795fd8fd65336e7498077e39ca
y8nju <aru0121052@gmail.com>
2023-01-31 17:45:43 +0900

    Add: User Video

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,47 @@
 const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getMedia() {
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            // 요청된 미디어 유형을 포함하는 트랙 을 생성하는 미디어 입력을 사용할 수 있는 권한을 사용자에게 요청
+            {
+                audio: true, 
+                video: true
+            }
+        );
+        // console.log(myStream);
+        myFace.srcObject = myStream;
+    } catch (e) {
+        console.log(e);
+    }
+}
+getMedia();
+
+function handleMuteClick(){
+    if(!muted) {
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+}
+function handleCameraClick(){
+    if(cameraOff) {
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }
+}
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
